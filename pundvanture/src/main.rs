610416a289b9c0d91efce46b4a08525d360e20d4
J-ZhengLi <lizheng135@huawei.com>
2023-05-16 20:02:47 +0800

    Add basic assets; init game window

--- pundvanture/src/main.rs
+++ pundvanture/src/main.rs
@@ -1,3 +1,32 @@
+mod plugins;
+
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    #[cfg(feature = "hot-reloading")]
+    let watch_for_changes = true;
+    #[cfg(not(feature = "hot-reloading"))]
+    let watch_for_changes = false;
+
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(AssetPlugin { watch_for_changes, ..default() })
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Pundvanture".into(),
+                        fit_canvas_to_parent: true,
+                        prevent_default_event_handling: true,
+                        ..default()
+                    }),
+                    ..default()
+                })
+        )
+        .add_startup_system(startup)
+        .run()
+}
+
+fn startup(mut cmd: Commands) {
+    cmd.spawn(Camera2dBundle::default());
 }
